947138c6aba677e008320c4e077aa81cf02955db
Вячеслав <slaer13@tut.by>
2021-11-21 17:22:48 +0300

    project V.1.2 fix bugs

--- backend/recipes/admin.py
+++ backend/recipes/admin.py
@@ -1,27 +1,42 @@
 from django.contrib import admin
 
-from .models import Favorite, Ingredient, Recipe, ShoppingList, Tag
+from .models import (Favorite, Ingredient, Recipe, RecipeIngredient,
+                     ShoppingList, Tag)
 
 
+@admin.register(Tag)
 class TagAdmin(admin.ModelAdmin):
-    list_display = ('name', 'slug', 'color')
+    list_display = ('pk', 'name', 'slug')
 
 
+@admin.register(Ingredient)
 class IngredientAdmin(admin.ModelAdmin):
-    list_display = ('name', 'measurement_unit')
-    list_filter = ('name',)
+    list_display = ('pk', 'name', 'measurement_unit')
+    list_filter = ['name']
+    search_fields = ('name',)
 
 
+class RecipeIngredientsInline(admin.TabularInline):
+    model = RecipeIngredient
+    min_num = 1
+    extra = 1
+
+
+@admin.register(Recipe)
 class RecipeAdmin(admin.ModelAdmin):
-    list_display = ('name', 'author', 'count_favorites')
-    list_filter = ('author', 'name', 'tags')
+    list_display = ('pk', 'name', 'author', 'in_favorite')
+    list_filter = ['name', 'author', 'tags']
+    inlines = (RecipeIngredientsInline,)
 
-    def count_favorites(self, obj):
+    def in_favorite(self, obj):
         return obj.in_favorite.all().count()
 
 
-admin.site.register(Tag, TagAdmin)
-admin.site.register(Ingredient, IngredientAdmin)
-admin.site.register(Recipe, RecipeAdmin)
-admin.site.register(ShoppingList)
-admin.site.register(Favorite)
+@admin.register(Favorite)
+class FavoriteAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'user', 'recipe')
+
+
+@admin.register(ShoppingList)
+class ShoppingListAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'user', 'recipe')
